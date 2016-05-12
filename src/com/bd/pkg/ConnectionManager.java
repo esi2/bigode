ad0e0c0887e3eabbde03f56fa6af1aa8bc203d70
FernandoFavorettigit config --global user.name FernandoFavoretti <fernandofvprado@gmail.com>
2016-05-12 09:56:45 -0300

    Bugode

--- src/com/bd/pkg/ConnectionManager.java
+++ src/com/bd/pkg/ConnectionManager.java
@@ -7,7 +7,7 @@ public class ConnectionManager {
 	private static ConnectionManager instance = null;
 
 	private final String USERNAME = "root";
-	private final String PASSWORD = "root";
+	private final String PASSWORD = "EACHesi2016!";
 	private final String CONN_STRING = "jdbc:mysql://localhost:3306/BIGODE";
 
 	private Connection conn = null;
