1ac9cdea5afe3cf54ebd2042fde4939ef0a2f3e8
CinthiaMV <cinthia.vlj@gmail.com>
2016-06-22 15:20:42 -0300

    Novos testes e descomentados

--- tests/EntrarMesaTest.java
+++ tests/EntrarMesaTest.java
@@ -13,29 +13,28 @@ public class EntrarMesaTest extends AbstractTests {
 		driver.get(BASE_URL);
 	}
 
-	//@Test
-	//public void testCodigoMesaValido() {
-	//	entrarCodigoMesa(CODIGO_MESA);
-	//	assertTrue(isElementPresent(By.id("submit-btn")));
-//	}
+	@Test
+	public void testCodigoMesaValido() {
+		entrarCodigoMesa(CODIGO_MESA);
+		assertTrue(isElementPresent(By.id("submit-btn")));
+	}
 	
-	//@Test
-	//public void testCodigoMesaVazio() {
-	//	entrarCodigoMesa("");
-	//	assertTrue(isElementPresent(By.xpath(mesaInvalidaXPath)));
-	//}
+	@Test
+	public void testCodigoMesaVazio() {
+		entrarCodigoMesa("");
+		assertTrue(isElementPresent(By.xpath(mesaInvalidaXPath)));
+	}
 	
-	/*@Test
+	@Test
 	public void testCodigoMesaInvalido() {
 		StringBuilder str = new StringBuilder();
 		for (int i=0; i<=5; i++) {
 			str.append(randomChar());
-		}
+	}
 		
 		entrarCodigoMesa(str.toString());
 		assertTrue(isElementPresent(By.xpath(mesaInvalidaXPath)));
 	}
-	*/
 
 	private void entrarCodigoMesa(String codigo) {
 		driver.findElement(By.id("codigoMesa")).clear();
@@ -46,5 +45,4 @@ public class EntrarMesaTest extends AbstractTests {
 	private char randomChar() {
 		return (char) randomInt(0x21, 0x7E);
 	}
-
 }
